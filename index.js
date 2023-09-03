9cdb68c0d595f239d45424e1be3717cefc6d73df
PallaviKhedle <pallavi8khedle@gmail.com>
2023-09-02 19:06:37 -0700

    changes

--- index.js
+++ index.js
@@ -19,3 +19,21 @@
  }
 
  const betterFunction=debounce(getData,300)
+
+
+
+
+//SIMPLE DEBOUNCE FUNCTION
+
+function debounce(fn, delay){
+    let timerId=null
+    return function(...args){
+        //clearTimeout(timerId) is called to clear any existing timeout that might have been set previously.
+        //This is important because if the function is called multiple times in quick succession, 
+        //the timer should reset to ensure that the debouncing behavior is maintained.
+        clearTimeout(timerId)
+        timerId=setTimeout(()=>fn.apply(this, args),delay)// new timeout is set
+
+    }
+}
+ 
